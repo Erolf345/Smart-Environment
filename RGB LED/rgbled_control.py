4b24f859cd13c7ad7e6c5c000daf61a4131d88c4
Dennis Gankin <41707311+DennisGankin@users.noreply.github.com>
2020-11-28 11:39:20 +0100

    Update rgbled_control.py

--- RGB LED/rgbled_control.py
+++ RGB LED/rgbled_control.py
@@ -15,9 +15,11 @@ led.color = Color("purple")
 sleep(1)
 led.toggle() #invert
 sleep(1)
-led.blink(on_time=1.5, off_time=0.5, fade_in_time=0.5, fade_out_time=0.5, on_color=(1, 0.3, 1), off_color=(0, 1, 0), n=10, background=True)
+led.blink(on_time=1.5, off_time=0.5, fade_in_time=0.5, fade_out_time=0.5, on_color=(1, 0.3, 1), off_color=(0, 1, 0), n=10, background=False)
+
 
 led.off()
 
-
+print("done")
+led.close()
 
