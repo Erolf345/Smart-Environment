fb45845384d1767a0f7ea6a1b67e0058d0f89b60
Erik Riedel <er9805@web.de>
2020-12-01 18:35:46 +0100

    Make ubuntu connect script executable

--- mesh_network/ubuntu_connect.sh
+++ mesh_network/ubuntu_connect.sh
@@ -1,14 +1,24 @@
+read -p "What is the name of your mesh interface: " mesh_if
+
+
 systemctl stop NetworkManager.service
 
-
-iw dev wlp0s20f3 del
-iw phy phy0 interface add wlp0s20f3 type ibss
-ip link set up mtu 1532 dev wlp0s20f3 #todo change this
-ifconfig wlp0s20f3 down
-iwconfig wlp0s20f3 mode ad-hoc essid my-mesh-network ap A6:00:28:01:5F:F8 channel 5
-ifconfig wlp0s20f3 up
-
-ifconfig bat0 169.254.100.100 netmask 255.255.0.0
-
-batctl if add wlp0s20f3
-
+sleep 1
+iw dev $mesh_if del
+sleep 1
+iw phy phy0 interface add $mesh_if type ibss
+sleep 1
+ip link set up mtu 1532 dev $mesh_if #todo change this
+sleep 1
+ifconfig $mesh_if down
+sleep 1
+iwconfig $mesh_if mode ad-hoc essid my-mesh-network ap A6:00:28:01:5F:F8 channel 5
+sleep 1
+ifconfig $mesh_if up
+sleep 1
+batctl if add $mesh_if
+sleep 1
+ifconfig bat0 169.254.100.100 netmask 255.255.0.0 # TODO: replace this with avahi
+sleep 1
+sudo sysctl net.ipv4.icmp_echo_ignore_broadcasts=0
+ping -c 2 -b 169.254.255.255
