b01be3239249c0860c3a857bbd8d33a1bbaed001
Erolf345 <E.p.riedel@gmail.com>
2020-12-01 11:07:40 +0000

    Prevent DHCPCD from auto configuring wlan0

--- mesh_network/init.sh
+++ mesh_network/init.sh
@@ -1,2 +1,5 @@
 sudo cp ./interfaces/bat0 /etc/network/interfaces.d/
 sudo cp ./interfaces/wlan0 /etc/network/interfaces.d/
+
+# Prevent DHCPCD from automatically configuring wlan0, THIS IS KEY
+echo 'denyinterfaces wlan0' | sudo tee --append /etc/dhcpcd.conf
