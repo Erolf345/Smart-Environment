8f1fed44e979bc043b79ab22688c24dba251293e
Erolf345 <E.P.Riedel@gmail.com>
2020-12-11 14:00:20 +0100

    ubuntu connection now works with dynamic cell ids, automatically detects phy iface, uses autoipd for ip adress negotiation

--- mesh_network/ubuntu_connect.sh
+++ mesh_network/ubuntu_connect.sh
@@ -1,24 +1,30 @@
+#!/bin/bash
 read -p "What is the name of your mesh interface: " mesh_if
 
+asdf=$(sudo iw dev | grep -B 1 "$mesh_if" | grep "phy")
+phys_if=$(awk '{gsub(/\#/,"")}1' <<< "$asdf")
 
 systemctl stop NetworkManager.service
 
 sleep 1
 iw dev $mesh_if del
 sleep 1
-iw phy phy0 interface add $mesh_if type ibss
+echo "$phys_if"
+iw phy "$phys_if" interface add $mesh_if type ibss
 sleep 1
 ip link set up mtu 1532 dev $mesh_if #todo change this
 sleep 1
 ifconfig $mesh_if down
 sleep 1
-iwconfig $mesh_if mode ad-hoc essid my-mesh-network ap A6:00:28:01:5F:F8 channel 5
+sudo batctl if add $mesh_if
 sleep 1
 ifconfig $mesh_if up
 sleep 1
-batctl if add $mesh_if
+ifconfig bat0 up
 sleep 1
-ifconfig bat0 169.254.100.100 netmask 255.255.0.0 # TODO: replace this with avahi
+iwconfig $mesh_if channel 3
 sleep 1
-sudo sysctl net.ipv4.icmp_echo_ignore_broadcasts=0
-ping -c 2 -b 169.254.255.255
+iw $mesh_if ibss join my-mesh-network 2432
+sleep 1
+sudo avahi-autoipd -D bat0
+echo "Now wait for avahi autoipd to generate ip adress"
